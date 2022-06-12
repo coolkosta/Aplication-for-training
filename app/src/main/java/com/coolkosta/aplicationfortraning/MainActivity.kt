5ee17ad30ca55f07fde71761de25a7b8056e0e2f
coolkosta <kostia.borzakov@gmail.com>
2022-06-12 13:46:55 +0300

    How to create ViewBinding

--- app/src/main/java/com/coolkosta/aplicationfortraning/MainActivity.kt
+++ app/src/main/java/com/coolkosta/aplicationfortraning/MainActivity.kt
@@ -1,49 +1,23 @@
 package com.coolkosta.aplicationfortraning
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.coolkosta.aplicationfortraning.databinding.ActivityMainBinding
 
-private const val KEY = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    /* инициализируем в Activity RecyclerView */
-    private lateinit var usersRecyclerView: RecyclerView
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val uerList: List<User> = listOf(User("Иван", 6),
-            User("Николай", 45),
-            User("Георгий", 34),
-            User("Игорь", 21),
-            User("Савелий", 30),
-            User("Егор", 67),
-            User("Константин", 55),
-            User("Александр", 76),
-            User("Михаил", 15),
-            User("Артем", 87),
-            User("Максим", 28),
-            User("Егор", 67),
-            User("Константин", 55),
-            User("Александр", 76),
-            User("Михаил", 15),
-            User("Артем", 87),
-            User("Максим", 28)
-        )
-
-
-        /* настраиваем RecyclerView
-        1. ищем по id необходимый RecyclerView
-        2. задаем RecyclerView параметры layOutManager-а путем настраивания отображения view
-        3. После создания ViewHolder-а и Adapter-а, присваеваем для RecyclerView Adapter */
-        usersRecyclerView = findViewById(R.id.user_recycler_view) // 1.
-        usersRecyclerView.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false) // 2.
-        usersRecyclerView.adapter = UserAdapter(uerList) // 3.
-
+        binding.textView.text = "Tap on me"
+        binding.textView.setOnClickListener {
+            Toast.makeText(this, "You pushing on TextView", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/coolkosta/aplicationfortraning/User.kt
+++
@@ -1,3 +0,0 @@
-package com.coolkosta.aplicationfortraning
-
-data class User(val name: String, val age: Int)
