f4a50a4f61dd56b5d3dcb72b438b9373acd5ade6
coolkosta <kostia.borzakov@gmail.com>
2022-06-15 21:49:45 +0300

    Learn how to work with Parcelize

--- app/src/main/java/com/coolkosta/aplicationfortraning/MainActivity.kt
+++ app/src/main/java/com/coolkosta/aplicationfortraning/MainActivity.kt
@@ -1,23 +1,55 @@
 package com.coolkosta.aplicationfortraning
 
 import android.os.Bundle
-import android.widget.Toast
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import com.coolkosta.aplicationfortraning.databinding.ActivityMainBinding
+import kotlinx.parcelize.Parcelize
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var state: State
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.button.text = getString(R.string.tap)
+        binding.button.setOnClickListener { tapped() }
 
-        binding.textView.text = "Tap on me"
-        binding.textView.setOnClickListener {
-            Toast.makeText(this, "You pushing on TextView", Toast.LENGTH_SHORT).show()
-        }
+        state = savedInstanceState?.getParcelable(KEY) ?: State(
+            0,
+            text = "tap on button"
+        )
+        render()
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY, state)
+    }
+
+
+    private fun render() = with(binding) {
+        textView.text = state.text
+    }
+
+
+    private fun tapped() {
+        state.count++
+        state.text = "U tapped on button ${state.count} times"
+        render()
+    }
+
+    @Parcelize
+    data class State(var count: Int, var text: String) : Parcelable
+
+    companion object {
+        @JvmStatic
+        private val KEY = "KEY"
     }
 }
