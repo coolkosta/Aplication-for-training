fec5331c8dabff3b6b69247c36cb9bf133aa86bb
coolkosta <kostia.borzakov@gmail.com>
2022-06-20 20:32:33 +0300

    Create AboutActivity, setup BaseActivity, work with Manifest.

---
+++ app/src/main/java/com/coolkosta/aplicationfortraning/AboutActivity.kt
@@ -0,0 +1,24 @@
+package com.coolkosta.aplicationfortraning
+
+import android.os.Bundle
+import com.coolkosta.aplicationfortraning.databinding.ActivityAboutBinding
+
+
+class AboutActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.versionNameTextView.text = BuildConfig.VERSION_NAME
+        binding.versionCodeTextView.text = BuildConfig.VERSION_CODE.toString()
+        binding.okButton.setOnClickListener { onOkPressed() }
+    }
+
+    private fun onOkPressed() {
+        finish()
+    }
+
+}
