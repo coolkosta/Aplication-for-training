dc4317b49115d5e8276dead552c136c299d63511
coolkosta <kostia.borzakov@gmail.com>
2022-07-01 15:12:05 +0300

    commit

--- app/src/main/java/com/coolkosta/aplicationfortraning/MainActivity.kt
+++ app/src/main/java/com/coolkosta/aplicationfortraning/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.coolkosta.aplicationfortraning
 
+import android.app.Activity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
+import com.coolkosta.aplicationfortraning.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val editTextLauncher = registerForActivityResult(StartActivityForResult()) {
+     result: ActivityResult -> if (result.resultCode == Activity.RESULT_OK) {
+         val intent = result.data
+
+    }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
     }
 }
